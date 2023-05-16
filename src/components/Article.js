86ef64ed334e241c8c479e6c2f30aff9ea47012b
Jasmine Jiang <jasminemazzarella@gmail.com>
2023-05-16 18:57:11 -0400

    adds bonus emoji feature

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,11 +1,23 @@
 import React from "react";
 
 function Article({title, date="January 1, 1970", preview, minutes}) {
+    const lessThan30Mins = minutes < 30;
+    const coffeeEmojis = [];
+    const bentoEmojis = [];
+    if (lessThan30Mins) {
+        for (let i=0; i < (Math.ceil(minutes/5) * 5) / 5; i++) {
+            coffeeEmojis.push("☕️");
+        }
+    } else {
+        for (let i=0; i < (Math.ceil(minutes/10) * 10) / 10; i++) {
+            bentoEmojis.push("🍱");
+        }
+    }
     return (
         <article>
             <h3>{title}</h3>
             <small>
-                {date} * {minutes} minute read
+                {date} * {lessThan30Mins ? coffeeEmojis : bentoEmojis} {minutes} minute read
             </small>
             <p>{preview}</p>
         </article>
